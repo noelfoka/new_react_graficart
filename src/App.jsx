ddf974d7ba90ec42941b9783cec90722af4a3fef
noelfoka <nomgnenoel@gmail.com>
2024-01-23 12:10:03 +0100

    add event listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 const title = 'Bonjour les gens'
 
 function App() {
+
+  const handleClick = (e) => {
+    console.log(e);
+    alert("j'ai cliqué sur le titre");
+  }
   return (
     <>
-      <h1 id="title" className="title">{title}</h1>
+      <h1 onClick={handleClick} id="title" className="title">{title}</h1>
       <input type='text' defaultValue={title} />
       <p>
         Lorem ipsum dolor sit amet, consectetor adipisicing elit. Aaccusamus, asperiores, aspernatur asumenda consequatur corporiseum exepturi id illum maiores minus nam nobis quo reiciendis ullam veritatis.
