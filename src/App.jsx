3a5eb1f36579e2ad611e5a6683e16465f12d8d8a
noelfoka <nomgnenoel@gmail.com>
2024-01-23 12:40:14 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-const title = 'Bonjour les gens'
+const title = 'Bonjour les gens';
+const style = {
+  color: "red",
+  backgroundColor: "blue",
+};
+const showTitle = false;
 
 function App() {
 
@@ -8,7 +13,7 @@ function App() {
   }
   return (
     <>
-      <h1 onClick={handleClick} id="title" className="title">{title}</h1>
+      <h1 onClick={handleClick} id="title" style={style} className="title">{title}</h1>
       <input type='text' defaultValue={title} />
       <p>
         Lorem ipsum dolor sit amet, consectetor adipisicing elit. Aaccusamus, asperiores, aspernatur asumenda consequatur corporiseum exepturi id illum maiores minus nam nobis quo reiciendis ullam veritatis.
