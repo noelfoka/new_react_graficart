cc8362ce5c2e09a057f5a8a043a20f6260c25b37
noelfoka <nomgnenoel@gmail.com>
2024-01-23 12:01:29 +0100

    add default value to input

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 id="title" className="title">{title}</h1>
-      <input type='text' />
+      <input type='text' defaultValue={title} />
       <p>
         Lorem ipsum dolor sit amet, consectetor adipisicing elit. Aaccusamus, asperiores, aspernatur asumenda consequatur corporiseum exepturi id illum maiores minus nam nobis quo reiciendis ullam veritatis.
       </p>
