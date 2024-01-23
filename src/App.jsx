451899b7d3cb46607a06bed008dc117faa9b5399
noelfoka <nomgnenoel@gmail.com>
2024-01-23 11:55:53 +0100

    add fragment

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ const title = 'Bonjour les gens'
 
 function App() {
   return (
-    <div>
+    <>
       <h1 id="title" className="title">{title}</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetor adipisicing elit. Aaccusamus, asperiores, aspernatur asumenda consequatur corporiseum exepturi id illum maiores minus nam nobis quo reiciendis ullam veritatis.
       </p>
-    </div>
+    </>
   )
 }
 
