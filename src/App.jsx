8c2ca205c1b77bd6d861fb1c0f5f830a865e36c9
noelfoka <nomgnenoel@gmail.com>
2024-01-24 07:21:47 +0100

    increment count

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => setCount(count + 1);
 
   return (
     <div>
-      <h1>Hello UseState</h1>
+      <p>{count}</p>
+      <button type="button" onClick={addCount}>increment</button>
     </div>
   );
 }
