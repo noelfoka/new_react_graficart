c5e6740c0479ef86a2d3e58af146b8bec1dd79d7
noelfoka <nomgnenoel@gmail.com>
2024-01-24 10:24:12 +0100

    add eventlistener to reset button

--- src/components/Formular.jsx
+++ src/components/Formular.jsx
@@ -7,11 +7,15 @@ const Formular = () => {
   const handleChange = (e) => {
     setFirstname(e.target.value);
   }
+
+  const reset = () => {
+    setFirstname('');
+  }
   return (
     <form>
         <input type="text" name="firstname" value={firstname} onChange={handleChange} />
         {firstname}
-        <button type="button" onClick={reset}></button>
+        <button type="button" onClick={reset}>Reset</button>
     </form>
   )
 }
