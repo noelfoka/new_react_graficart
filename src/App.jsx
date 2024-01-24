a0ad0154df7f918b6cf56671833b2fdaf0996d4e
noelfoka <nomgnenoel@gmail.com>
2024-01-24 08:14:11 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [person, setPerson] = useState({
+    firstName: "Noel",
+    lastName: "NOMGNE FOKA",
+    age: 15
+  });
 
   const addCount = () => setCount(count + 1);
 
@@ -9,6 +14,8 @@ function App() {
     <div>
       <p>{count}</p>
       <button type="button" onClick={addCount}>increment</button>
+      <p>Age de {person.firstName} {person.lastName}: {person.age}</p>
+      <button type="button" onClick={ajouter}>Ajouter une année</button>
     </div>
   );
 }
