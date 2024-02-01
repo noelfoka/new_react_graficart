94982aa54727b7c7025beff176e4857917816af5
noelfoka <nomgnenoel@gmail.com>
2024-02-01 21:18:20 +0100

    create Input file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import Formular from "./components/Formular"
-import Products from "./components/Products"
+const PRODUCTS = [
+    {category: "Fruits", price:"$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price:"$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price:"$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price:"$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price:"$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price:"$1", stocked: true, name: "Peas"}
+];
 
 function App() {
 
   return (
     <>
-      <Products />
-      <Formular />
+      
     </>
   )
 }
